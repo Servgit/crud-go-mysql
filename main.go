62778d8fd2a2879799abe4b18e35898cf54cb584
Servando_Lopez <servandolopez20@gmail.com>
2023-03-01 12:49:50 -0600

    agregamos un registro a la DB con un query en la func Inicio

--- main.go
+++ main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	//"log"
+	"database/sql"
 	"fmt"
 	"net/http"
 	"text/template"
 
 	_ "github.com/go-sql-driver/mysql"
 )
+
+func conexionDB() (conexion *sql.DB) {
+	Driver := "mysql"
+	Usuario := "root"
+	Contrasenia := "password123"
+	Nombre := "empleados_go"
+
+	conexion, err := sql.Open(Driver, Usuario+":"+Contrasenia+"@tcp(127.0.0.1)/"+Nombre)
+	if err != nil {
+		panic(err.Error())
+	}
+	return conexion
+}
 
 var plantillas = template.Must(template.ParseGlob("plantillas/*"))
 
@@ -19,6 +33,17 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 func Inicio(w http.ResponseWriter, r *http.Request) {
+
+	conexionEstablecida := conexionDB()
+
+	insertarRegistros, err := conexionEstablecida.Prepare("INSERT INTO empleados(nombre, email) VALUES('Pepito','pepe@gmail.com')")
+
+	if err != nil {
+		panic(err.Error())
+
+	}
+	insertarRegistros.Exec()
+
 	//fmt.Fprintf(w, "Hola Develoteca")
 	plantillas.ExecuteTemplate(w, "inicio", nil)
 }
